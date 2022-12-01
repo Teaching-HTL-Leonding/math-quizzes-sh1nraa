9586927277ac1fc0d4d3f0e9fe79bb85394d0e35
sh1nraa <113850100+sh1nraa@users.noreply.github.com>
2022-12-01 12:24:27 +0100

    Add files via upload

--- FindingOperator.cs
+++ FindingOperator.cs
@@ -1,1 +1,41 @@
-﻿
+﻿System.Console.Write("Number 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine()!);
+
+System.Console.Write("Number 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine()!);
+
+System.Console.Write("Result: ");
+int result = Convert.ToInt32(Console.ReadLine()!);
+
+string op = "";
+
+if(number1+number2 == result && number1 * number2 == result)
+{
+    System.Console.WriteLine($"Your operation was Plus and also Multiply");
+}
+else if(number1+number2 == result)
+{
+    op = "Plus";
+    System.Console.WriteLine($"Your operation was {op}");
+}
+else if(number1-number2 == result)
+{
+    op = "Minus";
+    System.Console.WriteLine($"Your operation was {op}");
+}
+else if(number1*number2 == result)
+{
+    op = "Multiply";
+    System.Console.WriteLine($"Your operation was {op}");
+}
+else if(number1 / number2 == result)
+{
+    op = "Divide";
+    System.Console.WriteLine($"Your operation was {op}");
+}
+else if(number1 % number2 == result)
+{
+    op = "Modulo";
+    System.Console.WriteLine($"Your Operation was {op}");
+}
+else{Console.Write("Error");}
