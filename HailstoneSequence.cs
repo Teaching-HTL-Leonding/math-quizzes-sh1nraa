8122f7ff69a0e01a6cd1bc7c68b12ee80b20d593
sh1nraa <113850100+sh1nraa@users.noreply.github.com>
2022-12-14 23:48:48 +0100

    Add files via upload

---
+++ HailstoneSequence.cs
@@ -0,0 +1,27 @@
+﻿//Hailstone sequence
+
+Console.Write("Enter your number: ");
+int number = Convert.ToInt32(Console.ReadLine()!);
+
+Console.Write($"{number}: {number} ");
+
+int counter = 0;
+
+while (number != 1)
+{
+    if (number % 2 == 0)
+    {
+        number = number / 2;
+    }
+    else
+    {
+        number = (number * 3) + 1;
+    }
+
+    Console.Write(number + " ");
+
+    counter++;
+}
+
+Console.WriteLine();
+Console.WriteLine($"The length of the sequence is {counter}");
